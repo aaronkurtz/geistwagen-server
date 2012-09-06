f6d0e752f034f3b464f7652473e156d9cc4f2f89
Aaron Kurtz <a.kurtz@hardsun.net>
2012-09-06 10:27:31 -0700

    add check for no bones available

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -22,6 +22,8 @@ def index():
 @get('/bones')
 def download():
   count = mongo_db.bones.count()
+  if 0==count:
+      abort(400, 'No bones exist')
   result = mongo_db.bones.find().limit(-1).skip(random.randrange(0,count)).next()
   return result
   
