dd5339929b63a520347c1adb538751bbcd4917bc
Aaron Kurtz <a.kurtz@hardsun.net>
2012-09-07 03:32:53 -0700

    Store md5sum, level and uploader IP

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -8,6 +8,7 @@ from bottle import abort
 import bson
 import pymongo
 import random
+import md5
 
 
 mongo_con = pymongo.Connection(
@@ -24,23 +25,27 @@ def index():
 
 @get('/bones')
 def download():
+  ip = request.headers['X-Forwarded-For']
   count = mongo_db.bones.count()
   if 0 == count:
       abort(400, 'No bones exist\n')
   result = mongo_db.bones.find().limit(-1).skip(random.randrange(0,count)).next()
   return str(result['file'])
+#TODO use Content Disposition to suggest filename
   
 
-@post('/bones')
+@post('/bones.<level>')
 def upload():
   data = request.body.readline()
-  logging.warning(request.headers.keys())
-  logging.warning(request.files.filename)
   if not data:
     abort(400, 'No data received\n')
   elif not (verify_bones_file(data)):
     abort(403, 'Bad data received\n')
-  document = {'file':bson.Binary(data)}
+  m = md5.new()
+  m.update(data)
+  md5sum = m.hexdigest()
+  ip = request.headers['X-Forwarded-For']
+  document = {'file':bson.Binary(data), 'ip':ip, 'md5':md5sum, 'level':level}
   mongo_db.bones.insert(document)
   return 'Uploaded bones file\n'
 
